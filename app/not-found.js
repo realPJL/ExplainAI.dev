dbce5d1cd0b547cf2f351af55ade09a966a455e1
PJL <pauljohannlorenz@proton.me>
2025-02-28 11:53:10 +0100

    Use <Link> instead of <a>

--- app/not-found.js
+++ app/not-found.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function NotFound() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
@@ -7,15 +9,15 @@ export default function NotFound() {
           Page not found
         </h1>
         <p className="mt-6 text-lg font-medium text-pretty text-gray-500 dark:text-gray-400 sm:text-xl/8">
-          Sorry, we couldn't find the page you're looking for.
+          Sorry, we could not find the page you are looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
+          <Link
             href="/"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
+          </Link>
           <a href="mailto:contact@explainai.dev" className="text-sm font-semibold text-gray-900 dark:text-gray-100">
             Contact support <span aria-hidden="true">&rarr;</span>
           </a>
